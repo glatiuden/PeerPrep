c5fff28aabb63ab63ca99e3cabb45153ac0cf7cf
Vinleon <kohvinleon@gmail.com>
2021-10-16 12:37:39 +0800

    Update local deploy script

--- backend/build.sh
+++ backend/build.sh
@@ -2,10 +2,10 @@
 aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 694217600744.dkr.ecr.ap-southeast-1.amazonaws.com
 for dir in */; 
 do 
-cd $dir
-name="${dir%/}"    
-docker build -t $name-ms:latest .
-docker tag $name-ms:latest 694217600744.dkr.ecr.ap-southeast-1.amazonaws.com/$name-ms:latest
-docker push 694217600744.dkr.ecr.ap-southeast-1.amazonaws.com/$name-ms:latest
-echo "Deployed to $name-MS"
+  cd $dir
+  name="${dir%/}"    
+  docker build -t $name-ms:latest .
+  docker tag $name-ms:latest 694217600744.dkr.ecr.ap-southeast-1.amazonaws.com/$name-ms:latest
+  docker push 694217600744.dkr.ecr.ap-southeast-1.amazonaws.com/$name-ms:latest
+  echo "Deployed to $name-MS"
 done
