58531c1ee97ccb086d6ee17f4fdfbe8c243801c9
Vinleon <kohvinleon@gmail.com>
2021-10-19 00:59:34 +0800

    Modify build script

--- backend/build.sh
+++ backend/build.sh
@@ -1,11 +1,22 @@
-# Used for CI/CD locally only
-aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 694217600744.dkr.ecr.ap-southeast-1.amazonaws.com
+# Used for local build only
+# aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 694217600744.dkr.ecr.ap-southeast-1.amazonaws.com
+# for dir in */; 
+# do 
+#   cd $dir
+#   name="${dir%/}"    
+#   docker buildx build --platform=linux/amd64 -t 694217600744.dkr.ecr.ap-southeast-1.amazonaws.com/$name-ms:latest .
+#   docker push 694217600744.dkr.ecr.ap-southeast-1.amazonaws.com/$name-ms:latest
+#   echo "Deployed to $name-MS"
+# done
 for dir in */; 
 do 
   cd $dir
   name="${dir%/}"    
-  docker buildx build --platform=linux/amd64 -t $name-ms:latest .
-  docker tag $name-ms:latest 694217600744.dkr.ecr.ap-southeast-1.amazonaws.com/$name-ms:latest
-  docker push 694217600744.dkr.ecr.ap-southeast-1.amazonaws.com/$name-ms:latest
-  echo "Deployed to $name-MS"
+  if [[ $name != "local-build" ]]
+  then
+    docker buildx build --platform=linux/amd64 -t 694217600744.dkr.ecr.ap-southeast-1.amazonaws.com/$name-ms:latest .
+    docker push 694217600744.dkr.ecr.ap-southeast-1.amazonaws.com/$name-ms:latest
+    echo "Deployed $name-MS"
+  fi
+  cd ../
 done
