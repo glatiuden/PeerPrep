3c7afa4557e80f64b2d74a6347bb0d0d136a545c
Vinleon <kohvinleon@gmail.com>
2021-10-22 21:46:00 +0800

    Add user stores

---
+++ user-frontend/store/user/actions.ts
@@ -0,0 +1,52 @@
+import _ from "lodash";
+import { ActionTree } from "vuex";
+import { ActionTypes } from "./action-type";
+import { MutationTypes } from "./mutation-type";
+import { UserState } from "./index";
+import { RootState } from "../index";
+import axios from "axios";
+
+const actions: ActionTree<UserState, RootState> = {
+  /**
+   * @description login as user
+   * @param param0
+   * @param param1
+   */
+  async [ActionTypes.LOGIN_USER](
+    { commit },
+    { email, password }: { email: string; password: string },
+  ) {
+    const { login_token, user } = await this.$axios.$post("/user/api/", {
+      email,
+      password,
+    });
+
+    localStorage.setItem("login_token", login_token);
+    commit(MutationTypes.SET_USER, { user });
+    if (user && login_token) {
+      commit(MutationTypes.SET_HAS_USER, { data: true });
+    }
+    return { login_token, user };
+  },
+
+  /**
+   * @description to get user by id
+   * @param param0
+   * @param param1
+   */
+  async [ActionTypes.GET_USER]({ commit }, { user_id }) {
+    const { data: user } = await this.$axios.$get(`/user/api/${user_id}`);
+    return user;
+  },
+  /**
+   * @description to create user
+   * @param param0
+   * @param param1
+   */
+  async [ActionTypes.CREATE_USER]({ commit }, { user }) {
+    const { data: created_user } = await this.$axios.$post(`/user/api/`, user);
+    return created_user;
+  },
+};
+
+export default actions;
