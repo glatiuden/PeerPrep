471e311e87d9c57c6eda4da72a79970032f064e3
Vinleon <kohvinleon@gmail.com>
2021-10-29 01:32:03 +0800

    Update build script

--- backend/build.sh
+++ backend/build.sh
@@ -1,5 +1,5 @@
 # Used for local build only
-# aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 694217600744.dkr.ecr.ap-southeast-1.amazonaws.com
+aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 694217600744.dkr.ecr.ap-southeast-1.amazonaws.com
 # for dir in */; 
 # do 
 #   cd $dir
