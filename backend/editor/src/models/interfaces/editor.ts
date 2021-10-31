25592052f2fc4b9bff2c5bdef279537962c86070
Vinleon <kohvinleon@gmail.com>
2021-10-31 12:27:52 +0800

    Test CI

--- backend/editor/src/models/interfaces/editor.ts
+++ backend/editor/src/models/interfaces/editor.ts
@@ -2,7 +2,7 @@ export default interface IEditor {
   _id: string;
   match_id: string;
   programming_language: string;
-  content: string; // Contains the codes
+  content: string;
   deleted_at?: Date;
   created_at: Date;
   updated_at: Date;
