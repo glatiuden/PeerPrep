a605a4ba8f0ff67139563cdfadd70b1a49f4ef6a
Vinleon <kohvinleon@gmail.com>
2021-11-01 18:06:11 +0800

    Minor fix to match test

--- backend/match/src/tests/test.ts
+++ backend/match/src/tests/test.ts
@@ -1,94 +1,57 @@
-process.env.NODE_ENV = "test";
-
+import _ from "lodash";
 import chai from "chai";
-import { expect } from "chai";
 import chaiHttp from "chai-http";
 import app from "../../index";
 
 const should = chai.should();
+const expect = chai.expect;
 chai.use(chaiHttp);
 
+let match_id = ""; // Used in retrieval & deletion test
 describe("GET_MATCHES", () => {
-  it("returns all matches", (done) => {
-      chai
-          .request(app)
-          .get("/match/admin")
-          .end((err, res) => {
-            should.exist(res.body);
-            res.should.have.status(200);
-            done();
-          });
+  it("returns all matches", async () => {
+    const res = await chai.request(app).get("/match/admin");
+    should.exist(res.body);
+    res.should.have.status(200);
   });
 });
 
 describe("CREATE_MATCH", () => {
-  it("creates a match", () => {
-      const match = {
-        partner1_id: "6169b3aa8b3147cb0a8d5000",
-        question_id: "6169b3aa8b3147cb0a8d5000",
-        status: "waiting",
-        programming_languages: ["java", "python"],
-        mode: "question",
-      };
-
-      chai
-          .request(app)
-          .post("match/api")
-          .send(match)
-          .end((err, res) => {
-            should.exist(res.body);
-            res.should.have.status(200);
-            res.body.partner1_id.should.be.eql(match.partner1_id);
-            res.body.question_id.should.be.eql(match.question_id);
-            res.body.match.should.have.property("programming_languages");
-            res.body.mode.should.be.eql(match.mode);
-          });      
-  });
-})
-
-describe("/DELETE_MATCH", () => {
-  let match : any;
-  before(async () => {
-    match = await  chai.request(app).post("/match/api").send({
-        partner1_id: "6169b3aa8b3147cb0a8d5000",
-        question_id: "6169b3aa8b3147cb0a8d5000",
-        status: "waiting",
-        programming_languages: ["java", "python"],
-        mode: "question"
-    })
-  })
-  it("deletes a match", async () => {
-    const id = match._id;
-    await chai.request(app).delete(`/match/api/${id}`).end((err, res) => {
-       res.should.have.status(200);
-    }); 
-
-  })
-})
-
-describe("/UPDATE_MATCH", () => {
-  let match : any;
-  before(async () => {
-    match = await  chai.request(app).post("/match/api").send({
-        partner1_id: "6169b3aa8b3147cb0a8d5000",
-        question_id: "6169b3aa8b3147cb0a8d5000",
-        status: "waiting",
-        programming_languages: ["java", "python"],
-        mode: "question"
-    })
-  })
-  it("updates a match", async () => {
-     const id = match._id;
-     await chai.request(app).put(`/match/api/${id}`).send({
+  it("creates a match", async () => {
+    const match = {
       partner1_id: "6169b3aa8b3147cb0a8d5000",
       question_id: "6169b3aa8b3147cb0a8d5000",
-      status: "completed",
-      programming_languages: ["java", "python"],
-      mode: "question"
-     }).end((err, res) => {
-        res.should.have.status(200);
-        res.body.status.should.be.eql("completed");
-     });     
-  })
-})
+      status: "waiting",
+      mode: "question",
+      programming_language: "java",
+    };
 
+    const res = await chai.request(app).post("/match/api").send(match);
+    should.exist(res.body);
+    expect(res).to.have.status(200);
+    expect(res.body).to.property("data").property("partner1_id", match.partner1_id);
+    expect(res.body).to.property("data").property("question_id", match.question_id);
+    expect(res.body).to.property("data").property("status", match.status);
+    expect(res.body).to.property("data").property("mode", match.mode);
+    match_id = _.get(res.body, "data._id");
+  });
+});
+
+describe("/UPDATE_MATCH", () => {
+  it("updates a match", async () => {
+    const res = await chai.request(app).put(`/match/api`).send({
+      _id: match_id,
+      partner2_id: "6169b3aa8b3147cb0a8d5000",
+      status: "completed",
+    });
+    res.should.have.status(200);
+    expect(res.body).to.property("data").property("status", "completed");
+  });
+});
+
+describe("/DELETE_MATCH", () => {
+  it("deletes a match", async () => {
+    const res = await chai.request(app).delete(`/match/admin/${match_id}`);
+    res.should.have.status(200);
+  });
+});
