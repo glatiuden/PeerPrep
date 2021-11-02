9cfe0aca4b73c98abe4bcc787bc2e01fd4dc4d3c
Vinleon <kohvinleon@gmail.com>
2021-11-02 22:39:18 +0800

    Add time taken

--- user-frontend/components/Session/BaseMatchDetails.vue
+++ user-frontend/components/Session/BaseMatchDetails.vue
@@ -19,6 +19,10 @@
           <br />
         </template>
       </v-countdown>
+      <span v-else>
+        <v-icon color="white" class="mr-1">mdi-timer</v-icon> Time Taken:
+        <b>{{ time_taken }} min</b>
+      </span>
       <v-divider class="my-6 white"></v-divider>
       <v-icon color="white" class="mr-1">mdi-account</v-icon>
       Partner's Display Name: <b>{{ partner_info.display_name }}</b>
@@ -30,7 +34,13 @@
       }}</b>
     </v-card-text>
     <v-card-actions class="pb-6 mx-2">
-      <v-btn color="error" block depressed @click="$emit('end-match')">
+      <v-btn
+        v-if="!isHistoryMode"
+        color="error"
+        block
+        depressed
+        @click="$emit('end-match')"
+      >
         End Match
       </v-btn>
     </v-card-actions>
@@ -80,6 +90,24 @@ export default {
       const time_left = end_time.diff(this.$moment(), "milliseconds");
       return time_left;
     },
+    /**
+     * @description Computes the exact duration left
+     */
+    time_taken() {
+      const updated_at = _.get(this.match, "updated_at");
+      const completed_at = _.get(this.match, "completed_at");
+      const recommended_duration = _.get(
+        this.match_question,
+        "recommended_duration",
+      );
+      const end_time = this.$moment(updated_at).add(
+        recommended_duration,
+        "minutes",
+      );
+      const time_taken = end_time.diff(completed_at, "minutes");
+      return time_taken;
+    },
+
     /**
      * @description Check whether should the timer be shown
      */
